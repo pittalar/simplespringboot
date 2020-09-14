5adcd980345c38bbc35997303655638928a8245d
Ramesh Pittala <ramesh.pittala@equinox.com>
2020-09-14 21:51:20 +0530

    added rest endpoint

--- src/main/java/com/java4u/demo/SimplespringbootApplication.java
+++ src/main/java/com/java4u/demo/SimplespringbootApplication.java
@@ -2,10 +2,18 @@ package com.java4u.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SimplespringbootApplication {
 
+	@GetMapping("/")
+	public String hello() {
+		return "hello world";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SimplespringbootApplication.class, args);
 	}
