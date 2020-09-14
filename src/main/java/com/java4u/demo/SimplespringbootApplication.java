b4fcbc2ab501cbab2c7fc5b24fae430aee053726
Ramesh Pittala <ramesh.pittala@equinox.com>
2020-09-14 15:25:17 +0530

    base code added

---
+++ src/main/java/com/java4u/demo/SimplespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.java4u.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java4u/demo/SimplespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java4u.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
